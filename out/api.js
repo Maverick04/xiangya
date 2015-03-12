20cac35f5beff79329fe082d62e6700a0c1f8865
Maverick04 <free.yangtong@gmail.com>
2015-03-12 10:11:48 +0800

    add interface documents

---
+++ out/api.js
@@ -0,0 +1,50 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ArgumentException",
+        "AuthDataModel",
+        "AuthRepositories",
+        "DepartmentDataModel",
+        "DepartmentRepositories",
+        "DoctorController",
+        "DoctorDataModel",
+        "DoctorRepositories",
+        "EntityRepositories",
+        "ForbiddenOperationException",
+        "PatientDataModel",
+        "PatientRepositories",
+        "RecordController",
+        "RecordDataModel",
+        "RecordEntityDataModel",
+        "RecordRepositories",
+        "ResourceNotFoundException",
+        "UserController",
+        "UserDataModel",
+        "UserRepositories"
+    ],
+    "modules": [
+        "Controllers",
+        "DataModels",
+        "Exceptions",
+        "Repositories"
+    ],
+    "allModules": [
+        {
+            "displayName": "Controllers",
+            "name": "Controllers"
+        },
+        {
+            "displayName": "DataModels",
+            "name": "DataModels"
+        },
+        {
+            "displayName": "Exceptions",
+            "name": "Exceptions"
+        },
+        {
+            "displayName": "Repositories",
+            "name": "Repositories"
+        }
+    ]
+} };
+});
