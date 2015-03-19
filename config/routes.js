5705573f24a9c0c8a59e2f59b1b5a38511d1cb42
Maverick04 <free.yangtong@gmail.com>
2015-03-19 08:18:36 +0800

    can get patient detail without auth

--- config/routes.js
+++ config/routes.js
@@ -32,7 +32,7 @@ module.exports = function(app){
    app.post("/user/resetPassword",auth.basicAuth,userController.resetPassword);
    app.post("/user/bindPatient",auth.basicAuth,userController.bindPatient);
    app.post("/user/unbindPatient",auth.basicAuth,userController.unbindPatient);
-   app.get("/user/patient/:patientid",auth.basicAuth,userController.getPatientById);
+   app.get("/user/patient/:patientid",userController.getPatientById);
    app.get("/user/patients",auth.basicAuth,userController.getAllPatients);
   
    //Doctor Accout Resource
